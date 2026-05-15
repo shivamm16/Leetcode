7689b80b02febcb48f44c8e0ed523e00f4bc6981
shivamm16 <shivamraj76678@gmail.com>
2026-05-15 22:52:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (67.78%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length(), len = 0;
+        boolean untilSpace = false;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                len++;
+                untilSpace = true;
+            } else if (untilSpace)
+                break;
+        }
+
+        return len;
+    }
+}
